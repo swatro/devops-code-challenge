7a1c0569f94f9bf81d410613a13084e27a6170fe
Stacey Watro <watro.stacey@gmail.com>
2018-02-22 15:11:31 -0500

    Adds logging and moves run app to an earlier hook.

--- scripts/installpython.sh
+++ scripts/installpython.sh
@@ -1,5 +1,11 @@
+echo "Installing python3.6"
+
 sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
 sudo yum -y update 
 sudo yum -y install python36
+
+echo "Successfully installed python36. Installing pip3"
 sudo easy_install-3.6 pip
+
+echo "Successfully installed pip3."
 
