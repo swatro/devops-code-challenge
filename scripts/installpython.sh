76be8d193c293aaba9b5ff352e42302899d99259
Stacey Watro <watro.stacey@gmail.com>
2018-02-22 12:24:40 -0500

    Fixes python install script.

--- scripts/installpython.sh
+++ scripts/installpython.sh
@@ -1,4 +1,5 @@
 sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-sudo yum -y install python36u
-sudo yum -y install python36u-pip
+sudo yum -y update 
+sudo yum -y install python36
+sudo easy_install-3.6 pip
 
