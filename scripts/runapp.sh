009b3938b9e6567bafd590451d3153541ba0135c
Stacey Watro <watro.stacey@gmail.com>
2018-02-22 16:39:42 -0500

    Adds full path for pip3 and flask.

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Install python dependencies via pip3. "
-pip3 install -r requirements.txt
+/usr/local/bin/pip3 install -r requirements.txt
 
 echo "Finished installing dependencies. Running application."
-FLASK_APP=chocolates_controller.py flask run --host=0.0.0.0
+FLASK_APP=chocolates_controller.py /usr/local/bin/flask run --host=0.0.0.0
