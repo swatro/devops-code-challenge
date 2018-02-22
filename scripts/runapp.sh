dde3e4b0d7403b96382dded5cbb02ad3e7badd82
Stacey Watro <watro.stacey@gmail.com>
2018-02-22 16:03:34 -0500

    Adds logging to start app script.

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -1,2 +1,5 @@
+echo "Install python dependencies via pip3. "
 pip3 install -r requirements.txt
+
+echo "Finished installing dependencies. Running application."
 FLASK_APP=chocolates_controller.py flask run --host=0.0.0.0
