7d80d4bd966479ce5c0565b96bff6a3954dc7113
Stacey Watro <watro.stacey@gmail.com>
2018-02-22 12:01:03 -0500

    Installs python and runs flask app.

---
+++ scripts/installpython.sh
@@ -0,0 +1,4 @@
+sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum -y install python36u
+sudo yum -y install python36u-pip
+
---
+++ scripts/runapp.sh
@@ -0,0 +1,2 @@
+pip3 install -r requirements.txt
+FLASK_APP=chocolates_controller.py flask run --host=0.0.0.0
