7c4527532a538f10f26678db9098a443795e98c6
Stacey Watro <watro.stacey@gmail.com>
2018-02-22 16:38:27 -0500

    Revert "Tries moving everything to one script."
    This reverts commit 4a8c294f90bb34a9ead7455ce57a07cafd798351.

--- scripts/installpython.sh
+++ scripts/installpython.sh
@@ -7,11 +7,7 @@ sudo yum -y install python36
 echo "Successfully installed python36. Installing pip3"
 sudo easy_install-3.6 pip
 
-echo "Successfully installed pip3. Install python dependencies via pip3. "
-pip3 install -r requirements.txt
-
-echo "Finished installing dependencies. Running application."
-FLASK_APP=chocolates_controller.py flask run --host=0.0.0.0
+echo "Successfully installed pip3."
 
 
 
