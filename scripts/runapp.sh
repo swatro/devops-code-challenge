58ff56667ffa1365074d8a42c3e21db4ade34303
Stacey Watro <watro.stacey@gmail.com>
2018-02-23 10:23:59 -0500

    Fixes python run command.

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -4,4 +4,4 @@ echo "Install python dependencies via pip3. "
 /usr/local/bin/pip3 install -r /home/code/requirements.txt
 
 echo "Finished installing dependencies. Running application."
-nohup /usr/local/bin/python3 /home/code/chocolates_controller.py &
+nohup python3 /home/code/chocolates_controller.py &
