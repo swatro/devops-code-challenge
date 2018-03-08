c526b99abc42cd2e9c67e88e92061e69df7facf8
Stacey Watro <watro.stacey@gmail.com>
2018-03-08 10:37:12 -0500

    Changing message to test deployment.

--- chocolates_controller.py
+++ chocolates_controller.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World Stacey! You are wonderful!"
 
 app.run(host='0.0.0.0', port=5000, debug=False)
 
